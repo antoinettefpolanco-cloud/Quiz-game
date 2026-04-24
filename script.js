58998960d4242720435d2a21e6392253f40dd203
antoinettefpolanco-cloud <antoinettefpolanco@gmail.com>
2026-04-24 15:49:43 +0000

    finished

--- script.js
+++ script.js
@@ -38,6 +38,12 @@ let userAnswers = [];
 let index = 0
 
 function showNextQuestion() {
+    // Check the answers
+    if (index >= questions.length) {
+        checkAnswers();
+        return;
+    }
+
     // TODO: Get the current question
     let question = questions[index];
     
@@ -78,18 +84,49 @@ function submitAnswer(event) {
 
     // TODO: Use a conditional (if/else-if) to check which option was clicked and push the option text into the userAnswer array
     if (option1.checked) {
-        console.log("option1 picked");
+        userAnswers.push(question.option1);
+        option1.checked = false;
+    }
+    else if (option2.checked) {
+        userAnswers.push(question.option2);
+        option2.checked = false;
+    }
+    else if (option3.checked) {
+        userAnswers.push(question.option3);
+        option3.checked = false;
+    }
+    else if (option4.checked) {
+        userAnswers.push(question.option4);
+        option4.checked = false;
+    }
+    else {
+        alert("You must select an asnwer");
+        return;
     }
 
     // TODO: Add one to the question index
+    index = index + 1;
 
     // TODO: Load the next question
+    showNextQuestion();
 }
 
 function checkAnswers() {
     // TODO: Iterate through the user answers array and count how many are correct
+    let numCorrect = 0;
+    for (let i = 0; i < userAnswers.length; i++) {
+        let userAnswer = userAnswers[i];
+        let question = questions[i];
+        if (userAnswer == question.answer) {
+            numCorrect ++;
+        }
+    }
 
     // TODO: Show an alert to the user with how many they got right out of the total
+    alert("You got " + numCorrect + " out of " + questions.length + " correct.");
 
     // TODO: Reset and start over
+    index = 0;
+    userAnswers = [];
+    showNextQuestion();
 }
